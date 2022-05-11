dee9a50a4ab9d1cfae50b732b3dad9a616aec39c
Helio de Jesus dos Santos Junior <84392613+HelioJr548@users.noreply.github.com>
2022-05-11 10:18:06 -0300

    Correção de variaveis e metodo

--- TRE/isset_post_cand.php
+++ TRE/isset_post_cand.php
@@ -1,21 +1,25 @@
 <?php
 
 
-if (isset($_POST['municipio']) and isset($_POST['partido']) and isset($_POST['candidato']) and isset($_POST['cargo'])) {
+if (isset($_POST['municipio']) and isset($_POST['UF']) and isset($_POST['partido']) and isset($_POST['sigla']) and isset($_POST['candidato']) and isset($_POST['cargo'])) {
 
     $municipio = $_POST['municipio'];
+    $uf = $_POST['UF'];
     $partido = $_POST['partido'];
+    $sigla = $_POST['sigla'];
     $candidato = $_POST['candidato'];
     $cargo = $_POST['cargo'];
     
 
-    $conteudo = "$municipio\n $partido\n $candidato\n $cargo\r\n";
+    $conteudo = "$municipio\n $uf\n $partido\n $sigla\n $candidato\n $cargo\r\n";
 
     $arquivo = fopen('isset_post_cand.txt', 'a+');
     fwrite($arquivo, $conteudo);
     fclose($arquivo);
     echo "Municipio: $municipio </br>
-    Partido: $partido </br> 
+    UF: $uf </br>
+    Partido: $partido </br>
+    Sigla: $sigla </br> 
     Candidato: $candidato </br> 
     Cargo: $cargo </br>";
 
