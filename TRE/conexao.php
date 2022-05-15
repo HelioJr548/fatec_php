93f174b6cae1972552dd3e372a1dbc4a78cac210
HelioJr548 <heliojr548@gmail.com>
2022-05-15 17:54:01 -0300

    Conexao com Banco de Dados

---
+++ TRE/conexao.php
@@ -0,0 +1,13 @@
+<?php 
+define('HOST', 'localhost');
+define('USUARIO', 'id18842383_root');
+define('SENHA',']<_N>P0{igr>kJEd');
+define('DB','id18842383_projeto_fatec');
+
+$conexao =mysqli_connect(HOST,USUARIO,SENHA,DB)
+or 
+die ('Não foi possivel conectar');
+
+echo "A conexão foi efetuada com sucesso!";
+
+?>
