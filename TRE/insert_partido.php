47d42c552aad3a01d0dccf593ee8d3b1875c899e
HelioJr548 <heliojr548@gmail.com>
2022-05-17 23:57:34 -0300

    Insert, Select e Delete

--- TRE/insert_partido.php
+++ TRE/insert_partido.php
@@ -22,27 +22,3 @@ die("Erro ao cadastrar os dados");
 echo "Dados cadastrados com sucesso!";
 
 ?>
-
-
-// $partido = $_POST['partido'];
-// $sigla = $_POST['sigla'];
-// $candidato = $_POST['candidato'];
-// $cargo = $_POST['cargo'];
-
-// $cpf = $_POST['cpf'];
-// $rg = $_POST['rg'];
-// $nome = $_POST['nome']
-
-// $sqlAlter = "ALTER TABLE users AUTO_INCREMENT = 1";
-// mysqli_query($conexao,$sqlAlter);
-
-// $sqlInserte =  "INSERT INTO Eleitor (Municipio,Zona,Secao,CPF,RG,Nome,) values ('$municipio', '$zona', '$secao', '$cpf', '$rg', $nome)";
-
-// $sqlInsertp = "INSERT INTO Partido (Partido,Sigla) values ('$partido', '$sigla')";
-
-// $sqlInsertc =  "INSERT INTO Candidato (Municipio,Partido,Candidato,Cargo) values ('$municipio', '$partido', '$candidato', '$cargo')";
-
-
-
-
-<!--mysql_query é uma função que verifica se os codigos sql estao corretos-->
