b0182d8993bd353b129336f3a433bc20505fce0a
Helio de Jesus dos Santos Junior <84392613+HelioJr548@users.noreply.github.com>
2022-06-07 22:48:47 -0300

    Criação do Database livraria

---
+++ AULAS/WebHost/MVC (Model-View-Controller)/database_livraria.sql
@@ -0,0 +1,10 @@
+CREATE TABLE livraria
+(
+    nome VARCHAR(255) NOT null,
+    autor VARCHAR(255),
+    quantidade int not null,
+    preco VARCHAR(255) NOT null,
+    flag tinyint(1) null DEFAULT 0,
+    data date not null,
+    PRIMARY KEY(nome)
+);
